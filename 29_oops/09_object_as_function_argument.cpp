b9f5ba66bb034a893ebd0d117e64a4555b27c0df
zainkhan456 <zainulislamk456@gmail.com>
2026-03-11 15:15:36 +0500

    Object as function arguments

--- 29_oops/09_object_as_function_argument.cpp
+++ 29_oops/09_object_as_function_argument.cpp
@@ -7,33 +7,33 @@ using namespace std;
 class Time{
 	int hours,minutes;
 	public:
-		void getdata(){
+		void setdata(){
 			cout<<"enter the value of hours: ";
 			cin>>hours;
 			cout<<"enter the value of minutes: ";
             cin>>minutes;
 		}
-		void setdata(){
+		void getdata(){
 			cout<<"\nhour: "<<hours<<endl;
 			cout<<"minutes: "<<minutes<<endl;
 		}
 
         //Function to calculate the total hours
 		void totalhours(Time t1,Time t2){
-			hours = (t1.minutes + t2.minutes)/60;//Add the minutes and convert it to hours
-			minutes = (t1.minutes + t2.minutes) % 60;//Add minutes
-			hours = hours + (t1.hours + t2.hours);//Calculate the total hours
+			hours = (t1.minutes + t2.minutes)/60; //Add the minutes and convert it to hours
+			minutes = (t1.minutes + t2.minutes) % 60; //Total minutes left
+			hours = hours + (t1.hours + t2.hours); //Calculate the total hours
 		}
 };
 int main()
 {
 	Time t1,t2,t3;
-	t1.getdata();
-	t2.getdata();
-	t3.totalhours(t1,t2);
 	t1.setdata();
 	t2.setdata();
-	t3.setdata();
+	t3.totalhours(t1,t2);
+	t1.getdata();
+	t2.getdata();
+	t3.getdata();
 return 0;
 }
 
